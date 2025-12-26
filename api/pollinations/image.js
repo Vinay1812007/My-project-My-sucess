fac236a37d26a323e37283020f3af9f00af1066d
Vinay1812007 <131197559+Vinay1812007@users.noreply.github.com>
2025-12-26 16:31:47 +0530

    Delete api/pollinations directory

--- api/pollinations/image.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    url: `https://image.pollinations.ai/prompt/${encodeURIComponent(req.query.prompt)}`
-  });
-}
--- api/pollinations/video.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    url: `https://video.pollinations.ai/prompt/${encodeURIComponent(req.query.prompt)}`
-  });
-}
