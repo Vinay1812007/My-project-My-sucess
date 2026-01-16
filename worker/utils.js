4a6b292e69a3469eaf3d76d307b12952d56bb995
Vinay1812007 <131197559+Vinay1812007@users.noreply.github.com>
2026-01-16 15:51:15 +0530

    utils.js

--- worker/utils.js
+++ worker/utils.js
@@ -1,3 +1,20 @@
-export function now() {
-  return new Date().toISOString();
+export function cors(body = null, status = 204) {
+  return new Response(body, {
+    status,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    }
+  });
+}
+
+export function json(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    }
+  });
 }
