919489ca21ab20e02e5dbe17d2cf0c1d3cb6f090
Vinay1812007 <131197559+Vinay1812007@users.noreply.github.com>
2026-01-16 18:25:28 +0530

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".card, .nav a").forEach((el) => {
-    el.addEventListener("touchstart", () => {
-      el.style.transform = "scale(0.97)";
-    });
-    el.addEventListener("touchend", () => {
-      el.style.transform = "";
-    });
-  });
-});
+console.log("Vinay Apps loaded");
